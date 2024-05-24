6c2ccc2fc48ab032063aa91ea5c156d2b17d8a07
marcosarias21 <marcosariasolaiz@gmail.com>
2024-05-24 18:53:40 -0300

    agregado legajo

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,7 +11,7 @@ const Footer = () => {
         <IconButton target="_blank" href="https://www.linkedin.com/in/marcos-ar/" color="primary"><LinkedInIcon /></IconButton>
       </Box>
       <Box>
-        <Typography variant="h6" fontWeight={'bold'}>@Marcos Arias - 2024</Typography>
+        <Typography variant="h6" fontWeight={'bold'} sx={{ ":hover": { color: '#1976d2',  transitionDuration: "0.25s" } }}>@Marcos Arias - 2024 - L:50665</Typography>
       </Box>
 
     </footer>
